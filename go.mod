cbcd5efa8ad254b2fac2cd0a3cebd20f9eaee39b
KaloyanYosifov <kaloqn665@gmail.com>
2020-06-28 18:34:47 +0300

    Add qt and add basic qt example

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/KaloyanYosifov/tricky-spotlight
 
 go 1.14
+
+require (
+	github.com/therecipe/qt v0.0.0-20200605184520-66b61001d1b1
+	github.com/therecipe/qt/internal/binding/files/docs/5.12.0 v0.0.0-20200605184520-66b61001d1b1 // indirect
+	github.com/therecipe/qt/internal/binding/files/docs/5.13.0 v0.0.0-20200605184520-66b61001d1b1 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/therecipe/qt/widgets"
+)
 
 func main() {
-	fmt.Println("Tricky spotlight")
+	// needs to be called once before you can start using the QWidgets
+	app := widgets.NewQApplication(len(os.Args), os.Args)
+
+	// create a window
+	// with a minimum size of 250*200
+	// and sets the title to "Hello Widgets Example"
+	window := widgets.NewQMainWindow(nil, 0)
+	window.SetMinimumSize2(1200, 900)
+	window.SetWindowTitle("Tricky Spotlight")
+
+	// create a regular widget
+	// give it a QVBoxLayout
+	// and make it the central widget of the window
+	widget := widgets.NewQWidget(nil, 0)
+	widget.SetLayout(widgets.NewQGridLayout2())
+	window.SetCentralWidget(widget)
+
+	// create a line edit
+	// with a custom placeholder text
+	// and add it to the central widgets layout
+	input := widgets.NewQLineEdit(nil)
+	input.SetPlaceholderText("Write something ...")
+	widget.Layout().AddWidget(input)
+
+	text := widgets.NewQLineEdit(nil)
+	widget.Layout().AddWidget(text)
+
+	// create a button
+	// connect the clicked signal
+	// and add it to the central widgets layout
+	button := widgets.NewQPushButton2("and click me!", nil)
+	button.ConnectClicked(func(bool) {
+		text.SetText(input.Text())
+	})
+	widget.Layout().AddWidget(button)
+
+	// make the window visible
+	window.Show()
+
+	// start the main Qt event loop
+	// and block until app.Exit() is called
+	// or the window is closed by the user
+	app.Exec()
 }

52142d1d1e052e947f4e38839811312a80f4fab2
KaloyanYosifov <kaloqn665@gmail.com>
2020-07-05 10:14:40 +0300

    Add indexing support

--- indexer.go
+++ indexer.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"github.com/KaloyanYosifov/tricky-spotlight/logger"
+	"github.com/KaloyanYosifov/tricky-spotlight/models"
 	"github.com/jinzhu/gorm"
 	"io/ioutil"
 	"regexp"
+	"strings"
 )
 
 func indexDesktopEntries(db *gorm.DB) {
@@ -25,16 +26,55 @@ func indexDesktopEntries(db *gorm.DB) {
 			continue
 		}
 
-		data, err := ioutil.ReadFile(linuxDesktopEntriesPath + file.Name())
+		desktopEntryPath := linuxDesktopEntriesPath + "/" + file.Name()
+		var desktopEntryFoundCount int
+
+		// check if we already have an entry with such path
+		db.Model(&models.DesktopEntry{}).Where("entry_path = ?", desktopEntryPath).Count(&desktopEntryFoundCount)
+		if desktopEntryFoundCount > 0 {
+			continue
+		}
+
+		data, err := ioutil.ReadFile(desktopEntryPath)
 
 		if err != nil {
 			logger.Debug("indexer: file couldn't be opened " + file.Name())
 			continue
 		}
 
-		fmt.Println("yes")
+		params := retrieveParamsFromEntries(strings.Split(string(data), "\n"))
+		executablePath, execOk := params["Exec"]
+		iconPath, iconOk := params["Icon"]
 
-		fmt.Println(data)
-		break
+		if !execOk || !iconOk {
+			logger.Debug("indexer: cannot retrieve either icon or executable paths")
+			continue
+		}
+
+		db.Create(&models.DesktopEntry{
+			Icon:           iconPath,
+			EntryPath:      desktopEntryPath,
+			ExecutablePath: executablePath,
+		})
+		continue
 	}
+}
+
+func retrieveParamsFromEntries(data []string) map[string]string {
+	params := map[string]string{}
+
+	for _, line := range data {
+		keyValuePairs := strings.Split(line, "=")
+
+		// if the keyValuePairs are less than 1
+		// then we haven't split by the = sign
+		// so in that case we haven't taken a param
+		if len(keyValuePairs) <= 1 {
+			continue
+		}
+
+		params[keyValuePairs[0]] = keyValuePairs[1]
+	}
+
+	return params
 }
